6c8ef935e859c8f7f0f6f156995b272f6d9774f4
tharunkumar-BIT <149181225+tharunkumar-BIT@users.noreply.github.com>
2025-04-28 19:00:17 +0530

    Login function

--- backend/controllers/userControllers.js
+++ backend/controllers/userControllers.js
@@ -1,32 +1,42 @@
 import { User } from "../models/userModels.js";
 import bcrypt from "bcrypt";
+import TryCatch from "../utils/TryCatch.js";
 
-export const registerUser = async (req, res) => {
-  try {
-    const { name, email, password } = req.body;
-
-    let user = await User.findOne({ email });
-
-    if (user)
-      return res.status(400).json({
-        message: "Already have an account with this email",
-      });
-
-    const hashPassword = await bcrypt.hash(password, 10);
-
-    user = await User.create({
-      name,
-      email,
-      password: hashPassword,
+export const registerUser = TryCatch(async (req, res) => {
+  const { name, email, password } = req.body;
+  let user = await User.findOne({ email });
+  if (user)
+    return res.status(400).json({
+      message: "Already have an account with this email",
     });
+  const hashPassword = await bcrypt.hash(password, 10);
+  user = await User.create({
+    name,
+    email,
+    password: hashPassword,
+  });
+  res.status(201).json({
+    user,
+    message: "User Registered",
+  });
+});
 
-    res.status(201).json({
-        user,
-        message: "User Registered"
-    })
-  } catch (error) {
-    res.status(500).json({
-      message: error.message,
+export const loginUser = TryCatch(async (req, res) => {
+  const { email, password } = req.body;
+  const user = await User.findOne({ email });
+  if (!user) {
+    return res.status(400).json({
+      message: "No user with this email",
     });
   }
-};
+  const comparePassword = await bcrypt.compare(password, user.password);
+  if (!comparePassword) {
+    return res.status(400).json({
+      message: "Wrong Password",
+    });
+  }
+  res.json({
+    user,
+    message: "Logged in successfully",
+  });
+});
--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,8 +1,9 @@
 import express from 'express'
-import { registerUser } from '../controllers/userControllers.js';
+import { loginUser, registerUser } from '../controllers/userControllers.js';
 
 const router = express.Router();
 
-router.post("/register", registerUser)
+router.post("/register", registerUser);
+router.post("/login", loginUser);
 
 export default router
