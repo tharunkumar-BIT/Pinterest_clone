39ed6423741ca1cdc1ebcba121d41ad20c6c9405
tharunkumar-BIT <149181225+tharunkumar-BIT@users.noreply.github.com>
2025-04-29 11:45:22 +0530

    configuring cloudinary

--- backend/index.js
+++ backend/index.js
@@ -1,10 +1,17 @@
-import express from 'express'
-import dotenv from 'dotenv';
-import connectDb from './database/db.js';
-import userRoutes from './routes/userRoutes.js'
-import cookieParser from 'cookie-parser';
+import express from "express";
+import dotenv from "dotenv";
+import connectDb from "./database/db.js";
+import userRoutes from "./routes/userRoutes.js";
+import cookieParser from "cookie-parser";
+import cloudinary from "cloudinary";
 
 dotenv.config();
+
+cloudinary.v2.config({
+  cloud_name: process.env.Cloud_Name,
+  api_key: process.env.Cloud_Api,
+  api_secret: process.env.Cloud_Secret,
+});
 
 const app = express();
 const PORT = process.env.PORT;
@@ -16,7 +23,7 @@ app.use(express.json());
 app.use(cookieParser());
 app.use("/api/user", userRoutes);
 
-app.listen(PORT, ()=> {
-    console.log(`Server is running in http://localhost:${PORT}`);
-    connectDb();
+app.listen(PORT, () => {
+  console.log(`Server is running in http://localhost:${PORT}`);
+  connectDb();
 });
