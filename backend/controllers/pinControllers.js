873769457567882f09f24a34345d3f0a17a26690
tharunkumar-BIT <149181225+tharunkumar-BIT@users.noreply.github.com>
2025-04-29 13:52:08 +0530

    creating a pin

--- backend/controllers/pinControllers.js
+++ backend/controllers/pinControllers.js
@@ -1,5 +1,26 @@
-import TryCatch from "../utils/TryCatch";
+import { Pin } from "../models/pinModel.js";
+import TryCatch from "../utils/TryCatch.js";
+import getDataUrl from "../utils/urlGenerator.js";
+import cloudinary from "cloudinary";
 
-export const createPin = TryCatch(async(req,res) => {
-    
-})
+export const createPin = TryCatch(async (req, res) => {
+  const { title, pin } = req.body;
+  const file = req.file;
+  const fileUrl = getDataUrl(file);
+
+  const cloud = await cloudinary.v2.uploader.upload(fileUrl.content);
+
+  await Pin.create({
+    title,
+    pin,
+    image: {
+      id: cloud.public_id,
+      url: cloud.secure_url,
+    },
+    owner: req.user._id,
+  });
+
+  res.json({
+    message: "Pin created",
+  });
+});
--- backend/routes/pinRoutes.js
+++ backend/routes/pinRoutes.js
@@ -1,5 +1,10 @@
 import express from "express";
+import { isAuth } from "../middlewares/isAuth.js";
+import uploadFile from "../middlewares/multer.js";
+import { createPin } from "../controllers/pinControllers.js";
 
 const router = express.Router();
+
+router.post("/new", isAuth, uploadFile, createPin)
 
 export default router;
