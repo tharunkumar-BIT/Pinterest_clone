01d5090ee0afb890928eda2a65dfb9f88c773d97
tharunkumar-BIT <149181225+tharunkumar-BIT@users.noreply.github.com>
2025-04-30 19:27:17 +0530

    fetch user data from backend

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,15 +2,17 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import Login from "./pages/Login";
 import Register from "./pages/Register";
+import { UserData } from "./context/userContext";
 
 const App = () => {
+  const { loading, isAuth, user } = UserData();
   return (
     <>
       <BrowserRouter>
         <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/login" element={<Login />} />
-          <Route path="/register" element={<Register />} />
+          <Route path="/" element={isAuth ? <Home /> : <Login />} />
+          <Route path="/login" element={isAuth ? <Home /> : <Login />} />
+          <Route path="/register" element={isAuth ? <Home /> : <Register />} />
         </Routes>
       </BrowserRouter>
     </>
--- frontend/src/context/UserContext.jsx
+++ frontend/src/context/UserContext.jsx
@@ -1,4 +1,4 @@
-import { useState, createContext, useContext } from "react";
+import { useState, createContext, useContext, useEffect } from "react";
 import toast, { Toaster } from "react-hot-toast";
 import axios from "axios";
 
@@ -8,9 +8,9 @@ export const UserProvider = ({ children }) => {
   const [user, setUser] = useState([]);
   const [isAuth, setIsAuth] = useState(false);
   const [btnLoading, setBtnLoading] = useState(false);
+
   async function loginUser(email, password, navigate) {
     setBtnLoading(true);
-
     try {
       const { data } = await axios.post("/api/user/login", { email, password });
       toast.success(data.message);
@@ -23,8 +23,28 @@ export const UserProvider = ({ children }) => {
       setBtnLoading(false);
     }
   }
+
+  const [loading, setLoading] = useState(true);
+
+  async function fetchUser() {
+    try {
+      const { data } = await axios.get("/api/user/me");
+      setUser(data);
+      setIsAuth(true);
+      setLoading(false);
+    } catch (error) {
+      console.log(error);
+      setLoading(false);
+    }
+  }
+  useEffect(() => {
+    fetchUser();
+  }, []);
+
   return (
-    <UserContext.Provider value={{ loginUser, btnLoading, isAuth, user }}>
+    <UserContext.Provider
+      value={{ loginUser, btnLoading, isAuth, user, loading }}
+    >
       {children}
       <Toaster />
     </UserContext.Provider>
