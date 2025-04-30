c8378cd57c8a7f9b42635c5946e5750d771475ba
tharunkumar-BIT <149181225+tharunkumar-BIT@users.noreply.github.com>
2025-04-30 13:36:26 +0530

    login page design

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 const Login = () => {
   return (
@@ -10,6 +11,53 @@ const Login = () => {
             alt="Pinterest"
             className="h-12"
           />
+        </div>
+        <h2 className="text-2xl font-semibold text-center mb-6">
+          Login to see more
+        </h2>
+        <form>
+          <div className="mb-4">
+            <label
+              htmlFor="email"
+              className="block text-sm font-medium text-gray-700"
+            >
+              Email
+            </label>
+            <input type="email" id="email" className="common-input" />
+          </div>
+          <div className="mb-4">
+            <label
+              htmlFor="password"
+              className="block text-sm font-medium text-gray-700"
+            >
+              Password
+            </label>
+            <input type="password" id="password" className="common-input" />
+          </div>
+          <button type="submit" className="common-btn">
+            Log in
+          </button>
+        </form>
+        <div className="mt-6 text-center">
+          <div className="relative mb-4">
+            <div className="absolute inset-0 flex items-center">
+              <div className="w-full border-t border-gray-300"></div>
+            </div>
+            <div className="relative flex justify-center text-sm">
+              <span className="bg-white px-2 to-gray-50">OR</span>
+            </div>
+          </div>
+          <div className="mt-4 text-center text-sm">
+            <span>
+              Not on Pinterest yet?{" "}
+              <Link
+                to="/register"
+                className="font-medium text-pinterest hover:underline"
+              >
+                Register
+              </Link>
+            </span>
+          </div>
         </div>
       </div>
     </div>
