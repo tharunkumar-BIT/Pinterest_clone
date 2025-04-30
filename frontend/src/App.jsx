574606a931b496537034020b56c7fb1f669fc116
tharunkumar-BIT <149181225+tharunkumar-BIT@users.noreply.github.com>
2025-04-30 11:36:09 +0530

    pages folder

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,9 +1,20 @@
-import React from 'react'
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import Login from "./pages/Login";
+import Register from "./pages/Register";
 
 const App = () => {
   return (
-    <div className='text-red-600'>App</div>
-  )
-}
+    <>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/register" element={<Register />} />
+        </Routes>
+      </BrowserRouter>
+    </>
+  );
+};
 
-export default App
+export default App;
---
+++ frontend/src/pages/Login.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+
+const Login = () => {
+  return (
+    <div className="min-h-screen flex items-center justify-center bg-gray-100">
+      <div className="bg-white p-8 rounded-lg shadow-lg w-full max-w-md">
+        <div className="flex justify-center mb-4">
+          <img
+            src="https://upload.wikimedia.org/wikipedia/commons/0/08/Pinterest-logo.png"
+            alt="Pinterest"
+            className="h-12"
+          />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Login;
