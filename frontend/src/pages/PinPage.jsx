8ccfa71f49e37be38e0d3645a01be7e7c607ee68
tharunkumar-BIT <149181225+tharunkumar-BIT@users.noreply.github.com>
2025-05-01 22:46:03 +0530

    individual pin page

--- frontend/src/pages/PinPage.jsx
+++ frontend/src/pages/PinPage.jsx
@@ -1,6 +1,7 @@
 import React, { useEffect } from "react";
 import { useParams } from "react-router-dom";
 import { PinData } from "../context/PinContext";
+import { Loading } from "../components/Loading";
 
 const PinPage = () => {
   const params = useParams();
@@ -9,7 +10,27 @@ const PinPage = () => {
   useEffect(() => {
     fetchPin(params.id);
   }, [params.id]);
-  return <div>PinPage</div>;
+  return (
+    <div>
+      {pin && (
+        <div className="flex flex-col items-center bg-gray-100 p-4 min-h-screen">
+          {loading ? (
+            <Loading />
+          ) : (
+            <div className="bg-white rounded-lg shadow-lg flex flex-wrap w-full max-w-4xl">
+              <div className="w-full md:w-1/2 bg-gray-200 rounded-t-lg md:rounded-l-lg md:rounded-t-none flex items-center justify-center">
+                <img
+                  src={pin.image.url}
+                  alt=""
+                  className="object-cover w-full rounded-t-lg md:rounded-l-lg md:rounded-t-none"
+                />
+              </div>
+            </div>
+          )}
+        </div>
+      )}
+    </div>
+  );
 };
 
 export default PinPage;
