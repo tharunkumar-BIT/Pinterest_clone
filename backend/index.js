986dd58a07e2c25e12068f7cc8fcaba8b975cb04
tharunkumar-BIT <149181225+tharunkumar-BIT@users.noreply.github.com>
2025-05-18 13:25:23 +0530

    backend change

--- backend/index.js
+++ backend/index.js
@@ -33,7 +33,7 @@ app.use("/api/pin", pinRoutes);
 // Static frontend in production
 if (process.env.NODE_ENV === "production") {
   app.use(express.static(path.join(__dirname, "/frontend/dist")));
-  app.get("*", (req, res) => {
+  app.get("/:path(*)", (req, res) => {
     res.sendFile(path.join(__dirname, "frontend", "dist", "index.html"));
   });
 }
